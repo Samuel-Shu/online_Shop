5e4602b99a3923ae14b59b4303b090927a0e33b2
ShuCoding <2385970514@qq.com>
2025-03-17 17:08:57 +0800

    `update README`

--- online_Shop_grpc/llms_srv/config/config.py
+++ online_Shop_grpc/llms_srv/config/config.py
@@ -5,9 +5,9 @@ import os
 load_dotenv()
 
 # OpenAI API配置
-OPENAI_API_BASE = "https://ms-fc-7963d553-445c.api-inference.modelscope.cn/v1"
-OPENAI_API_KEY = "077335bc-7b0e-42c3-8162-3d8dbce56f61"
-MODEL_ID = "Qwen/Qwen2-7B-Instruct-GGUF"
+OPENAI_API_BASE = ""
+OPENAI_API_KEY = ""
+MODEL_ID = "DeepSeekR1"
 
 # 向量数据库配置
 CHROMA_PERSIST_DIRECTORY = "./data/chroma_db"
